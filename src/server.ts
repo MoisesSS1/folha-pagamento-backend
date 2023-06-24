a66bcbb794070089b6f2c86153f85098841d3130
Moises <investmentmoises@gmail.com>
2023-06-24 01:23:43 -0300

    atualizando portttttt

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import express from "express";
 import userRoutes from "./routes/userRoutes"
 import cors from "cors"
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 
